0e712c6150d8ee2cc2ba25da91d782cb5a3e1565
renan <renan.r@outlook.com.br>
2018-07-24 15:20:17 -0300

    exercicio coversão temperatura add celsius para Farenheit

--- EstruturaSequencial/ConverteTemperatura.py
+++ EstruturaSequencial/ConverteTemperatura.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 
 grausF = input ("Informe a quantidade de graus em Farenheit: ")
-grausC = (5*(grausF-32)/9)
+grausCelsius = (5*(grausF-32)/9)
 
-print "%s graus Farenheit e igual a %s em graus Celsius" %(grausF,grausC)
+grausC = input ("Informe a quantidade de graus em Celsius: ")
+p1 = ((grausC / 5)*9)+32
+
+print "%s graus Farenheit e igual a %s em graus Celsius" %(grausF,grausCelsius)
+print "%s graus Celsius e igual a %s em graus Farenheit" %(grausC,p1)
+
+
+
+
+
+#(grausC / 5) = (grausFarenheit - 32) / 9
