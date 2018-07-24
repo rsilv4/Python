d3e61fc27db2445d54cd015822bad61249fa43cf
renan <renan.r@outlook.com.br>
2018-07-24 14:39:24 -0300

    exercicio calculo area quadrado

---
+++ EstruturaSequencial/AreaQuadrado.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+lado = input ("Informe a altura do quadrado: ")
+area = lado**2
+dobroarea = area * 2
+
+print "A area do quadrado e: %s" %area
+print "O dobro da area informada acima e: %s" %dobroarea
