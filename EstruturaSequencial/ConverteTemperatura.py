ec15223c74f2c75a216c035ecf328e62c59d97e5
renan <renan.r@outlook.com.br>
2018-07-24 15:04:38 -0300

    execicio converte temperatura

---
+++ EstruturaSequencial/ConverteTemperatura.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+grausF = input ("Informe a quantidade de graus em Farenheit: ")
+grausC = (5*(grausF-32)/9)
+
+print "%s graus Farenheit e igual a %s em graus Celsius" %(grausF,grausC)
