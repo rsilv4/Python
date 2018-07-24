cfa99d39d4828bcc0955cfb3723b50ed5bf7acf4
renan <renan.r@outlook.com.br>
2018-07-24 20:41:27 -0300

    Calculo salario com impostos

--- EstruturaSequencial/CalculoSalario.py
+++ EstruturaSequencial/CalculoSalario.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
-salariohora = input ("Informa quanto ganha por hora: ")
-horastrab = input ("Informe a quantidade de horas trabalhadas no mes: ")
+print "++++ Calcula Salario ++++"
 
-salario = salariohora * horastrab
+IR=0.11
+INSS=0.08
+SIND=0.05
 
-print "Seu salario do mes sera de: %s" %salario
+CustoHora = input ("Informe o custo da hora trabalhada: ")
+HorasTrab = input ("Informe a quantidade de horas trabalhadas no mes: ")
 
+SalarioBruto = CustoHora * HorasTrab
+ValorDescIR = SalarioBruto * IR
+ValorDescINSS = SalarioBruto * INSS
+ValorDescSind = SalarioBruto * SIND
+TotalDesc = (ValorDescIR + ValorDescINSS + ValorDescSind)
+
+SalarioLiquido = SalarioBruto - TotalDesc
+
+print "Salario Bruto : R$ %s" %SalarioBruto
+print "IR (11) : R$ %s" %ValorDescIR
+print "INSS (8) : R$ %s" %ValorDescINSS
+print "Sindicato (5) : R$ %s" %ValorDescSind
+print "Salario Liquido : R$ %s" %SalarioLiquido
