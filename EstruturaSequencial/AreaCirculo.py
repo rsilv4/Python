a45c55a8cac17190c502925301fd1c275a869af6
renan <renan.r@outlook.com.br>
2018-07-24 14:29:10 -0300

    Execicio calculo area circulo

---
+++ EstruturaSequencial/AreaCirculo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+PI=3.1415
+raio = input("Informe o raio do circulo que deseja saber a area: ")
+
+area = (raio**2) * PI
+print "A area de um circulo com raio de %s e de %s aproximadamente" %(raio,area)
