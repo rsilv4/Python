e6ebb8c856848e811907cb2711d5d9aefe0e6c6d
renan <renan.r@outlook.com.br>
2018-07-24 14:02:45 -0300

    Exercicio ConverteMetrosemCm

---
+++ EstruturaSequencial/ConverteMetrosemCm.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+print "Programa para converter metros em centimetros"
+metros = input ("Informe a quantidade de metros que deseja converter: ")
+
+vcentimentros = metros * 100
+
+print "O valor de %s metros em centimetros e: %s" %(metros,vcentimentros)
