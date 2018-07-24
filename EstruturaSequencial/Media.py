6400257bdc977310f07a001d96931740c761b0b1
renan <renan.r@outlook.com.br>
2018-07-24 13:45:20 -0300

    exercicio 3 - MediaNotas

---
+++ EstruturaSequencial/Media.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+nota1 = input ("Informe a primera nota: ")
+nota2 = input ("Informe a segunda nota: ")
+nota3 = input ("Informe a terceira nota: ")
+nota4 = input ("Informe a quarta nota: ")
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print "A media das 4 notas e: ", media
