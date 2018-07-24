0a51570c7c22f02f1e9136a1cf8f6c0d721d2c83
renan <renan.r@outlook.com.br>
2018-07-24 14:52:59 -0300

    execicio calculo salario simples

---
+++ EstruturaSequencial/CalculoSalario.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+salariohora = input ("Informa quanto ganha por hora: ")
+horastrab = input ("Informe a quantidade de horas trabalhadas no mes: ")
+
+salario = salariohora * horastrab
+
+print "Seu salario do mes sera de: %s" %salario
+
