a57a00ff7a8c3fb6a803be11ebfc948ab87cb3a9
renan <renan.r@outlook.com.br>
2018-07-24 15:26:17 -0300

    alteração nome variavel p1 para grausFarenheit

--- EstruturaSequencial/ConverteTemperatura.py
+++ EstruturaSequencial/ConverteTemperatura.py
@@ -4,13 +4,7 @@ grausF = input ("Informe a quantidade de graus em Farenheit: ")
 grausCelsius = (5*(grausF-32)/9)
 
 grausC = input ("Informe a quantidade de graus em Celsius: ")
-p1 = ((grausC / 5)*9)+32
+grausFarenheit = ((grausC / 5)*9)+32
 
 print "%s graus Farenheit e igual a %s em graus Celsius" %(grausF,grausCelsius)
-print "%s graus Celsius e igual a %s em graus Farenheit" %(grausC,p1)
-
-
-
-
-
-#(grausC / 5) = (grausFarenheit - 32) / 9
+print "%s graus Celsius e igual a %s em graus Farenheit" %(grausC,grausFarenheit)
