fb90f412704b67ebdaa64c518943dc1e51d26892
renan <renan.r@outlook.com.br>
2018-07-24 20:11:35 -0300

    Rendimento diario com if

--- EstruturaSequencial/RendimentoDiario.py
+++ EstruturaSequencial/RendimentoDiario.py
@@ -6,10 +6,10 @@ PESOESTAB=50
 VMULTA=4.00
 
 peso = float (input("Informe o peso dos peixeis: "))
-excesso = (peso - PESOESTAB)
-
-multa = excesso * VMULTA
-
-print "Peso informado: %s" %peso
-print "Peso excedido: %s" %excesso
-print "Valor da multa: %s" %multa
+if peso > PESOESTAB:
+    excesso = (peso - PESOESTAB)
+    
+    multa = excesso * VMULTA
+    print "O peso informado foi de %s, foi execedido %s quilos e o valor da multa sera de %s" %(peso,excesso,multa)
+else:
+    print "O peso informado e de %s, nao existe multa a se pagar" %peso
