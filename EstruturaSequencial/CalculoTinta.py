d71aa51c6b1070d183f9bf8827f3c8955df907bd
renan <renan.r@outlook.com.br>
2018-07-24 21:52:54 -0300

    exercicio calculo tinta com preço

--- EstruturaSequencial/CalculoTinta.py
+++ EstruturaSequencial/CalculoTinta.py
@@ -4,6 +4,7 @@ print "++++ Calculo de Tinta ++++"
 
 REND=3
 TAMLATA=18
+VALORLATA=18.00
 
 Metragem = float (input ("Informe o tamanho da area a ser pintada em Metro quadrados: "))
 QtdLitrosTinta = Metragem / REND
@@ -11,5 +12,9 @@ QtdLatas = round((QtdLitrosTinta / TAMLATA),0)
 
 if QtdLatas < 1.0 :
     print "Sera necessario %s Litros de tinta, voce precisa comprar 1 Lata de 18 Litros" %QtdLitrosTinta
+    Total = VALORLATA
+    print "Valor a ser pago: R$%s"%Total
 else:
     print "Sera necessario %s Litros de tinta, voce precisa comprar %s latas de 18 Litros para essa metragem" %(QtdLitrosTinta,float(QtdLatas))
+    Total = QtdLatas * VALORLATA
+    print "Valor a ser pago: R$%s"%Total
