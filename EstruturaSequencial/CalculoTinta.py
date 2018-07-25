cf6f22edad1b91d7e95d3def1142d89846763c9a
renan <renan.r@outlook.com.br>
2018-07-24 21:45:47 -0300

    exercicio calculo tinta

---
+++ EstruturaSequencial/CalculoTinta.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print "++++ Calculo de Tinta ++++"
+
+REND=3
+TAMLATA=18
+
+Metragem = float (input ("Informe o tamanho da area a ser pintada em Metro quadrados: "))
+QtdLitrosTinta = Metragem / REND
+QtdLatas = round((QtdLitrosTinta / TAMLATA),0)
+
+if QtdLatas < 1.0 :
+    print "Sera necessario %s Litros de tinta, voce precisa comprar 1 Lata de 18 Litros" %QtdLitrosTinta
+else:
+    print "Sera necessario %s Litros de tinta, voce precisa comprar %s latas de 18 Litros para essa metragem" %(QtdLitrosTinta,float(QtdLatas))
